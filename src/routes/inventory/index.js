5e1eea32eb833911ede1bef9259d14d1c0dd506f
CODA29 <rasdagm@gmail.com>
2025-12-02 00:34:08 -0800

    feat: built an API to read an inventory item by id

--- src/routes/inventory/index.js
+++ src/routes/inventory/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const Ajv = require('ajv');
+const createError = require('http-errors');
 const router = express.Router();
+const { Inventory } = require('../../models/inventory');
 
-// routes...
-router.get('/', (req, res) => res.send("inventory ok"));
+router.get('/:itemId', async (req, res, next) => {
+    try{
+        const inventoryItem = await Inventory.findOne({ itemId: Number(req.params.itemId) });
+
+        if(!inventoryItem){
+            return next(createError(404, 'Inventory item not found'));
+        }
+
+        console.log('Result: ', inventoryItem);
+        res.json(inventoryItem);
+    }catch(err){
+        console.error(`Error while getting inventory: ${err}`);
+        next(err);
+    }
+});
 
 module.exports = router;
